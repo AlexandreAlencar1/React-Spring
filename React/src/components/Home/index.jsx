c24d05cbacb33068d731b2dbe6d45c9fdaeab83a
AlexandreAlencar1 <alexandre.s.alencar@hotmail.com>
2023-09-12 20:11:37 -0400

    add react/create spring

---
+++ React/src/components/Home/index.jsx
@@ -0,0 +1,72 @@
+import { useCallback, useEffect, useState } from "react";
+import { loadPosts } from "../../utils/load-posts";
+import { Button } from "../Button";
+import { PostCard } from "../PostCard";
+import "./styles.css";
+
+//-------------------------------------------------------------------------------------------------------
+export const Home = () => {
+const [posts, setPosts] = useState ([])
+const [allPosts, setAllPosts] = useState ([])
+const [page, setPage] = useState (0)
+const [postsPerPage] = useState (5)
+const [searchValue, setSearchValue] = useState ("")
+
+//----------------------------------------------------------------------------------------------------
+const filteredPosts = !!searchValue
+? posts.filter((post) => {
+  return post.title.toLowerCase().includes(searchValue.toLowerCase());
+})
+: posts;
+
+//----------------------------------------------------------------------------------------------------
+
+ const handleLoadPosts = useCallback( async (page, postsPerPage) => {
+    const photosAndPosts = await loadPosts();
+
+    setPosts(photosAndPosts.slice(page, postsPerPage))
+    setAllPosts(photosAndPosts)
+  },[]);
+
+//-------------------------------------------------------------------------------------------------------
+
+  const loadMorePosts = () => {
+    const nextPage = page + postsPerPage;
+
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+
+    setPosts([...posts, ...nextPosts])
+    setPage(nextPage);
+  };
+
+//-------------------------------------------------------------------------------------------------------
+  const handleSearch = (e) => { 
+    const {value} = e.target;
+    setSearchValue(value);
+  };
+//-------------------------------------------------------------------------------------------------------
+  
+useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  },[handleLoadPosts, postsPerPage]);   
+//-------------------------------------------------------------------------------------------------------
+
+  return (
+    <section className="container">
+        <input 
+        type="text"
+        id="txtSearch" 
+        name="name" 
+        placeholder="Search..." 
+        onChange={handleSearch} 
+        value={searchValue} />
+        
+      <div className="posts">
+        {filteredPosts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+      <Button text="Load more posts" action={loadMorePosts} />
+    </section>
+  );
+}
